247571d97f32b56a6ceccf74dbf39cf3467a2fc4
braden <leachbr787@northwested.org>
2025-04-23 13:26:25 -0400

    Update main.py

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ apple_pos = [random.randint(0, SCREEN_WIDTH // CELL_SIZE - 1) * CELL_SIZE, rando
 font = pygame.font.SysFont(None, 35)
 
 # -- Load and Play Background Music -- # 
-pygame.mixer.music.load('') # - Loads Music
+pygame.mixer.music.load("C:\Project-Text-Menu\mixkit-infected-vibes-157.mp3") # - Loads Music
 pygame.mixer.music.set_volume(0.5) # - Set volume to 50%
 pygame.mixer.music.play(-1) # - Plays Music in Loop
 
@@ -87,7 +87,54 @@ while running:
                 direction = 3
             elif event.key == pygame.K_LEFT and direction != 2: # - Left 
                 direction = 4
+    # -- Add Timer -- # 
+    if update_snake > 99: 
+        update_snake = 0 
 
+        # -- Move The Snake -- # 
+        head_x, head_y = snake_pos[0]
 
+        if direction == 1: # - Up 
+            head_y -= CELL_SIZE
+        elif direction == 2: # - Right  
+            head_x += CELL_SIZE
+        elif direction == 3: # - Down
+            head_y += CELL_SIZE
+        elif direction == 2: # - Left  
+            head_x -= CELL_SIZE
 
+        # -- Update Snake Position -- # 
+        snake_pos.insert(0, [head_x, head_y]) # - Add New Head 
+        snake_pos.pop() # - Remove Last Segment 
 
+        # -- Check For Collision (Apple) -- # 
+        if snake_pos[0] == apple_pos: 
+            apple_pos[random.randint(0, SCREEN_WIDTH // CELL_SIZE - 1) * CELL_SIZE, random.randint(0, SCREEN_HEIGHT // CELL_SIZE - 1)]
+            snake_pos.append(snake_pos[-1]) # -  Add New Segment to Snake 
+            score += 1  # - Increase Score When Apple Eaten 
+        
+        # -- Check for Collision (Walls) -- #
+        if head_x < 0 or head_x >= SCREEN_WIDTH or head_y < 0 or head_y >= SCREEN_HEIGHT: 
+            running = False # - Exit game 
+
+        # -- Draw Snake -- # 
+        for i in range(len(snake_pos)): 
+            segment = snake_pos[i]
+            if i == 0: # - Head 
+                pygame.draw.rect(screen, BODY_OUTER, (segment[0], segment[1], CELL_SIZE, CELL_SIZE))
+                pygame.draw.rect(screen, RED, (segment[0] + 1, segment[1] + 1, CELL_SIZE - 2, CELL_SIZE - 2))
+            else: # - Body 
+                pygame.draw.rect(screen, BODY_OUTER, (segment[0], segment[1], CELL_SIZE, CELL_SIZE))
+                pygame.draw.rect(screen, BODY_INNER, (segment[0] + 1, segment[1] + 1, CELL_SIZE - 2, CELL_SIZE - 2))
+
+        # -- Update Display -- # 
+        pygame.display.flip()
+
+        update_snake += 1 
+
+# -- Exit Pygame -- # 
+pygame.quit()
+sys.exit()
+
+        
+        
